117224e741b9b0366f285ba55ce4c99ab0eaafa8
Daniel Bateman <jetison.24@gmail.com>
2012-06-17 03:12:42 -0500

    aries-common: Use .txt extract method
    Each device should extract their own files and then
    cd to aries-common and execute it's extract-files
    script.
    
    Change-Id: Iecf4e077d6a8bbeb52dcb84dfb4311807458a917

---
+++ extract-files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VENDOR=samsung
+DEVICE=aries-common
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh

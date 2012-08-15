1f1189e1a36127f361cfea1ee64f113fd56380ef
Daniel Bateman <jetison.24@gmail.com>
2012-08-15 01:05:14 -0500

    aries-common: Add sanity checking
    Purpose: Verify the integrity of select things including the IMEI.
             The IMEI specifically, if invalid, could cause emergency
             calling issues (i.e. inability to call at all).
    
    Warn the user if any issues are detected. Currently only the IMEI
    is checked against the generic IMEI (004999010640000).
    
    Change-Id: I8fb76b7535cc7110142b72892cf9d4fc1cbc25e7

--- AriesParts/src/com/cyanogenmod/settings/device/Startup.java
+++ AriesParts/src/com/cyanogenmod/settings/device/Startup.java
@@ -3,8 +3,14 @@ package com.cyanogenmod.settings.device;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class Startup extends BroadcastReceiver {
+    private static final String TAG = "AriesParts";
+    private static final String BAD_IMEI[] = {
+        "004999010640000"
+    };
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
@@ -14,6 +20,31 @@ public class Startup extends BroadcastReceiver {
         Hspa.restore(context);
         VolumeBoostPreference.restore(context);
         DockAudio.restore(context);
+        sanityCheck(context);
     }
 
+    private void sanityCheck(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(
+                Context.TELEPHONY_SERVICE);
+        String id = tm.getDeviceId();
+        if (tm.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM && !ensureIMEISanity(id)) {
+            Log.e(TAG, "Invalid IMEI!");
+            Intent intent = new Intent(context, WarnActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(WarnActivity.KEY_REASON, WarnActivity.REASON_INVALID_IMEI);
+            context.startActivity(intent);
+            return;
+        }
+        Log.d(TAG, "Device is sane.");
+    }
+
+    private boolean ensureIMEISanity(String id) {
+        Log.d(TAG, "Current IMEI: " + id);
+        for (int j = 0; j < BAD_IMEI.length; j++) {
+            if (BAD_IMEI[j].equals(id)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

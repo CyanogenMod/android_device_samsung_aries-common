06716ce73daaa54e0b635fe8751c1a17497e48a1
jt1134 <jt1134@gmail.com>
2012-09-22 23:15:39 -0500

    use logical volumes for system and data, bring back datadata
    * use lvm to split eMMC into 2 logical partitions
    * use entire available space on OneNAND for /datadata
    
    based on information from: http://bit.ly/OMny8Z
    
    WARNING: WILL WIPE DATA
    
    Change-Id: I3caeec096a6798afafaf03b2eb72c20580272682

--- updater.sh
+++ updater.sh
@@ -29,14 +29,14 @@ if /tmp/busybox test "$1" = cdma ; then
     # CDMA mode
     IS_GSM='/tmp/busybox false'
     SD_PART='/dev/block/mmcblk1p1'
-    DATA_PART='/dev/block/mmcblk0p1'
-    DATA_SIZE='490733568'
+    MMC_PART='/dev/block/mmcblk0p1'
+    MTD_SIZE='490733568'
 else
     # GSM mode
     IS_GSM='/tmp/busybox true'
     SD_PART='/dev/block/mmcblk0p1'
-    DATA_PART='/dev/block/mmcblk0p2'
-    DATA_SIZE='442499072'
+    MMC_PART='/dev/block/mmcblk0p2'
+    MTD_SIZE='442499072'
 fi
 
 # check for old/non-cwm recovery.
@@ -88,7 +88,8 @@ if /tmp/busybox test -e /dev/block/bml7 ; then
     /sbin/reboot now
     exit 0
 
-elif /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/size` != "$DATA_SIZE" ; then
+elif /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE" || \
+    /tmp/busybox test `/tmp/busybox cat /sys/class/mtd/mtd2/name` != "datadata" ; then
     # we're running on a mtd (old) device
 
     # make sure sdcard is mounted
@@ -183,13 +184,23 @@ elif /tmp/busybox test -e /dev/block/mtdblock0 ; then
     # remove the cyanogenmod.cfg to prevent this from looping
     /tmp/busybox rm -f /sdcard/cyanogenmod.cfg
 
-    # unmount and format system (recovery seems to expect system to be unmounted)
+    # unmount system and data (recovery seems to expect system to be unmounted)
     /tmp/busybox umount -l /system
-    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /system $DATA_PART
-
-    # unmount and format data
     /tmp/busybox umount -l /data
-    /tmp/erase_image userdata
+
+    # setup lvm volumes
+    /lvm/sbin/lvm pvcreate $MMC_PART
+    /lvm/sbin/lvm vgcreate lvpool $MMC_PART
+    /lvm/sbin/lvm lvcreate -L 400M -n system lvpool
+    /lvm/sbin/lvm lvcreate -l 100%FREE -n userdata lvpool
+
+    # format data and system
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /system /dev/lvpool/system
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /data /dev/lvpool/userdata
+
+    # unmount and format datadata
+    /tmp/busybox umount -l /datadata
+    /tmp/erase_image datadata
 
     # restart into recovery so the user can install further packages before booting
     /tmp/busybox touch /cache/.startrecovery

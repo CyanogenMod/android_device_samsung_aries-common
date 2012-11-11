f91a3acbe7829022c938d9ce905cef66a75b8d27
jt1134 <jt1134@gmail.com>
2012-11-11 03:07:34 -0600

    AriesParts: add a reset button for all seekbar preferences
    Change-Id: Ie554b1a1b2e0a6e907843882c978bfeae075eaff

--- AriesParts/src/com/cyanogenmod/settings/device/ColorTuningPreference.java
+++ AriesParts/src/com/cyanogenmod/settings/device/ColorTuningPreference.java
@@ -7,6 +7,8 @@ import android.preference.DialogPreference;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,7 +16,7 @@ import android.widget.TextView;
  * Special preference type that allows configuration of both the ring volume and
  * notification volume.
  */
-public class ColorTuningPreference extends DialogPreference {
+public class ColorTuningPreference extends DialogPreference implements OnClickListener {
 
     enum Colors {
         RED,
@@ -92,6 +94,13 @@ public class ColorTuningPreference extends DialogPreference {
             TextView valueDisplay = (TextView) view.findViewById(GAMMA_VALUE_DISPLAY_ID[i]);
             mSeekBars[SEEKBAR_ID.length + i] = new GammaSeekBar(seekBar, valueDisplay, GAMMA_FILE_PATH[i]);
         }
+
+        SetupButtonClickListener(view);
+    }
+
+    private void SetupButtonClickListener(View view) {
+        Button mResetButton = (Button)view.findViewById(R.id.color_reset);
+        mResetButton.setOnClickListener(this);
     }
 
     @Override
@@ -208,6 +217,16 @@ public class ColorTuningPreference extends DialogPreference {
             mValueDisplay.setText(String.format("%.3f", (double) progress / MAX_VALUE));
         }
 
+        public void resetDefault(String type, String path, int value) {
+            mSeekBar.setProgress(value);
+            updateValue(value);
+            if (type == "color") {
+                Utils.writeColor(path, value);
+            } else {
+                Utils.writeGamma(path, value);
+            }
+        }
+
     }
 
     class GammaSeekBar extends ColorSeekBar {
@@ -239,4 +258,18 @@ public class ColorTuningPreference extends DialogPreference {
         }
 
     }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.color_reset:
+                for (int i = 0; i < SEEKBAR_ID.length; i++) {
+                    mSeekBars[i].resetDefault("color", FILE_PATH[i], MAX_VALUE);
+                }
+                for (int i = 0; i < GAMMA_SEEKBAR_ID.length; i++) {
+                    mSeekBars[SEEKBAR_ID.length + i].resetDefault("gamma", GAMMA_FILE_PATH[i], GAMMA_DEFAULT_VALUE);
+                }
+                break;
+        }
+    }
+
 }

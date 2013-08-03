94499fe21486f0ccfde0b65a9052803845873666
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-08-03 21:59:32 +0700

    aries-common: set ro.bq.gpu_to_cpu_unsupported
    This tells SurfaceFlinger to always use the CPU
    pipeline for taking screenshot. We don't actually need
    this since our surface type isn't supported by Bitmap
    and will defaults to CPU anyway, but setting it doesn't
    hurt.
    
    Change-Id: I35c15dff969a89990c0f4ef6512cb79ab3c71362
    Signed-off-by: Pawit Pornkitprasan <p.pawit@gmail.com>

--- device_base.mk
+++ device_base.mk
@@ -160,6 +160,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
        ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        ro.vold.switchablepair=/mnt/emmc,/mnt/sdcard \
+       ro.bq.gpu_to_cpu_unsupported=1 \
 
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \

b9ee210c4a1b2f5b9d97aaae30dcccaf3504195b
Alberto96 <albertoguanti@gmail.com>
2013-08-03 14:44:20 +0200

    Set ro.bq.gpu_to_cpu_unsupported for aries
    Change-Id: I35c15dff969a89990c0f4ef6512cb79ab3c71362

--- device_base.mk
+++ device_base.mk
@@ -162,6 +162,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        ro.vold.switchablepair=/mnt/emmc,/mnt/sdcard \
 
+# GPU producer to CPU consumer not supported
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.bq.gpu_to_cpu_unsupported=1
+
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
 PRODUCT_PROPERTY_OVERRIDES += \

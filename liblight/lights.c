b38bf7f96e799343c4c4b9e4a589ffd8641dfa70
Liran Chen <liran.chen@gmail.com>
2013-09-14 12:59:25 +0300

    aries-common: Enhanch liblights to support notification flash Simulates device support for notification flashing since not supported directly by the hardware
    Change-Id: I5bd6f63c797129b8a69a82bd7b2edf00638e6779

--- liblight/lights.c
+++ liblight/lights.c
@@ -18,12 +18,16 @@
 #define LOG_TAG "lights"
 #include <cutils/log.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <hardware/lights.h>
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
@@ -31,6 +35,20 @@ static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const *const LCD_FILE = "/sys/class/backlight/s5p_bl/brightness";
 char const *const LED_FILE = "/sys/class/misc/notification/led";
+
+char const *const WAKE_LOCK_PATH   = "/sys/power/wake_lock";
+char const *const WAKE_UNLOCK_PATH = "/sys/power/wake_unlock";
+char const *const WAKE_LOCK_NAME   = "notification_flash_timer";
+
+#define FLASH_TIMER_CMD_ON 1
+#define FLASH_TIMER_CMD_OFF 0
+
+static timer_t g_flash_timer_id;
+static struct itimerspec g_flash_timer;
+static int g_flash_mode;
+static struct timespec g_flash_on_time;
+static struct timespec g_flash_off_time;
+static int g_flash_cmd;
 
 static int write_int(char const *path, int value)
 {
@@ -65,22 +83,120 @@ static int rgb_to_brightness(struct light_state_t const *state)
 		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
 }
 
+static void notifications_flash_callback(int signum)
+{
+    int v = 0;
+
+    pthread_mutex_lock(&g_lock);
+
+    if(LIGHT_FLASH_TIMED == g_flash_mode)
+    {
+       if(FLASH_TIMER_CMD_ON == g_flash_cmd)
+       {
+            g_flash_timer.it_value = g_flash_on_time;
+            g_flash_cmd = FLASH_TIMER_CMD_OFF;
+            v = 1;
+       }
+       else
+       {
+            g_flash_timer.it_value = g_flash_off_time;
+            g_flash_cmd = FLASH_TIMER_CMD_ON;
+            v = 0;
+       }
+
+        timer_settime(g_flash_timer_id, 0, &g_flash_timer, NULL);
+    }
+    else
+        v = 0;
+
+    ALOGD("notifications flash: v %u" , v);
+    write_int(LED_FILE, v);
+
+    pthread_mutex_unlock(&g_lock);
+}
+
+static struct timespec to_timespec(int total_ms)
+{
+    struct timespec ts;
+    ts.tv_sec = total_ms / 1000;
+    ts.tv_nsec = 1000000 * (total_ms % 1000);
+
+    return ts;
+}
+
+static void set_wakelock(bool acquire)
+{
+    int fd;
+    const char *const path = acquire ? WAKE_LOCK_PATH : WAKE_UNLOCK_PATH;
+    
+    fd = open(path, O_WRONLY);
+
+    if(fd != -1)
+    {
+        write(fd, WAKE_LOCK_NAME, strlen(WAKE_LOCK_NAME));
+        close(fd);
+    }
+}
+
 static int set_light_notifications(struct light_device_t* dev,
 			struct light_state_t const* state)
 {
-	int brightness =  rgb_to_brightness(state);
+	int brightness = 0;
 	int v = 0;
 	int ret = 0;
-	pthread_mutex_lock(&g_lock);
+	
+	brightness =  rgb_to_brightness(state);
+        if (brightness+state->color == 0 || brightness > 100) {
+		    if (state->color & 0x00ffffff)
+			    v = 1;
+	    } else
+		    v = 0;
+	
+	ALOGI("notifications: fMode %u, onMS %u, offMS %u, v %u", state->flashMode,
+	    state->flashOnMS, state->flashOffMS, v);
+	    
+    pthread_mutex_lock(&g_lock);
 
-	if (brightness+state->color == 0 || brightness > 100) {
-		if (state->color & 0x00ffffff)
-			v = 1;
-	} else
-		v = 0;
+    if(LIGHT_FLASH_TIMED == state->flashMode &&
+       state->flashOffMS > 0 && state->flashOnMS > 0)
+    {
+        if(LIGHT_FLASH_TIMED != g_flash_mode)
+            set_wakelock(true);
+        
+        g_flash_mode = LIGHT_FLASH_TIMED;
+    
+        g_flash_on_time = to_timespec(state->flashOnMS);
+        g_flash_off_time = to_timespec(state->flashOffMS);
 
-	ALOGI("color %u fm %u status %u is lit %u brightness", state->color, state->flashMode, v, (state->color & 0x00ffffff), brightness);
-	ret = write_int(LED_FILE, v);
+        g_flash_cmd = FLASH_TIMER_CMD_OFF;
+
+        g_flash_timer.it_value = g_flash_on_time;
+
+        ALOGD("set notifications flash: onMS %u, offMS %u", state->flashOnMS, state->flashOffMS);
+        
+        timer_settime(g_flash_timer_id, 0, &g_flash_timer, NULL);
+
+        ret = write_int(LED_FILE, 1);
+    }
+    else
+    {
+        if(LIGHT_FLASH_TIMED == g_flash_mode)
+        {
+            if(0 == v)
+            {
+                g_flash_mode = LIGHT_FLASH_NONE;
+
+                set_wakelock(false);
+            }
+        }
+    
+	    ALOGD("notifications: v %u", v);
+        ret = write_int(LED_FILE, v);
+    }
+
+    
+    g_flash_mode = state->flashMode;
+
 	pthread_mutex_unlock(&g_lock);
 	return ret;
 }
@@ -128,6 +244,8 @@ static int close_lights(struct light_device_t *dev)
 static int open_lights(const struct hw_module_t *module, char const *name,
 						struct hw_device_t **device)
 {
+    struct sigaction sa;
+    
 	int (*set_light)(struct light_device_t *dev,
 		struct light_state_t const *state);
 
@@ -136,7 +254,16 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
 	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
+	{
+	    sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        sa.sa_handler = notifications_flash_callback;
+        sigaction (SIGALRM, &sa, NULL);
+
+        timer_create(CLOCK_MONOTONIC, NULL, &g_flash_timer_id);
+
 		set_light = set_light_notifications;
+    }
 #ifdef BUTTONS_FILE
 	else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
 		set_light = set_light_buttons;

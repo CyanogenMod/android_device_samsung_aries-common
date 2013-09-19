065fec0661e54063469e472587b02332fdb3e1cb
jt1134 <jt1134@gmail.com>
2013-09-19 14:10:19 -0500

    aries-common: ifdef to allow inheriting from fascinatemtd
    - allows removal of lots of duplicated code in the fascinatemtd
      device tree.
    
    Change-Id: I2ed4cd215401191352c7a6fa012b996dafeea1ab

--- device_base.mk
+++ device_base.mk
@@ -157,9 +157,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
        wifi.interface=wlan0 \
        ro.telephony.ril_class=SamsungExynos3RIL \
-       ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
-       ro.vold.switchablepair=/mnt/emmc,/mnt/sdcard \
        ro.bq.gpu_to_cpu_unsupported=1 \
 
 # SGX540 is slower with the scissor optimization enabled
@@ -184,9 +182,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vold.umsdirtyratio=20
 
+# The following are not needed for some devices
+ifneq ($(VARIANT_MODEL), SCH-I500)
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock
+
 # We have sacrificed /cache for a larger /system, so it's not large enough for dalvik cache
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-data-only=1
+endif
 
 # Set default USB interface and default to internal SD as /sdcard
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
